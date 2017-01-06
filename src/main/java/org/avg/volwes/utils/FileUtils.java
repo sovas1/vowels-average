e50dfcebab7d022cca960820428846fc995bdf06
sovas <mariuszsowa11@gmail.com>
2017-01-06 21:11:32 +0100

    file utils implementation

--- src/main/java/org/avg/volwes/utils/FileUtils.java
+++ src/main/java/org/avg/volwes/utils/FileUtils.java
@@ -3,14 +3,20 @@ package org.avg.volwes.utils;
 import org.avg.volwes.model.VolwesInWord;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileUtils {
 
     public List<String> readWords(final String path) throws IOException {
-        return new ArrayList<>();
+        return Files.lines(Paths.get(path))
+                .map(line -> line.split("[\\s]+"))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
     }
 
     public void write(Stream<VolwesInWord> volvesInWorldStream) {
